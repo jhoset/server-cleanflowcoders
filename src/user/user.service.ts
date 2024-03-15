e57fc06bb41a4280a580bbaa0b309b93c484c79f
Luis Murrugarra <luis@combativa.com>
2024-03-14 23:32:28 -0500

    feat: add filter in users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,33 @@
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PaginationDto, PaginationResultDto } from 'src/common/dto';
-import { CreateUserDto, UpdatePasswordDto, UpdateUserDto, UserDto, UserWithRolesDto } from './dto';
+import {
+  CreateUserDto,
+  UpdatePasswordDto,
+  UpdateUserDto,
+  UserDto,
+  UserWithRolesDto,
+} from './dto';
 import { ConfigService } from '@nestjs/config';
 import { generateUserName } from 'src/helpers';
 import { BcryptAdapter } from 'src/common/adapters/bcrypt.adapter';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-
   private readonly serverUrl: string;
   constructor(
     private _prisma: PrismaService,
     private _configService: ConfigService,
-    private _bcryptAdapter: BcryptAdapter) {
+    private _bcryptAdapter: BcryptAdapter,
+  ) {
     this.serverUrl = this._configService.get<string>('serverUrl');
   }
-
 
   async create(createUserDto: CreateUserDto) {
     //TODO: GET CURRENT USER
@@ -25,7 +36,10 @@ export class UserService {
     await this.checkExistingRoles(roleIds);
 
     createUserDto.password = this._bcryptAdapter.hash(createUserDto.password);
-    const userName = generateUserName(createUserDto.firstName, createUserDto.lastName);
+    const userName = generateUserName(
+      createUserDto.firstName,
+      createUserDto.lastName,
+    );
 
     const userCreated = await this._prisma.user.create({
       data: {
@@ -33,38 +47,70 @@ export class UserService {
         userName,
         changedBy: 'system',
         roles: {
-          create: roleIds.map(id => (
-            {
-              changedBy: 'system',
-              role: { connect: { id } }
-            }
-          ))
-        }
-      }
-    })
+          create: roleIds.map((id) => ({
+            changedBy: 'system',
+            role: { connect: { id } },
+          })),
+        },
+      },
+    });
     return UserDto.mapFrom(userCreated);
   }
 
-  async findAll(paginationDto: PaginationDto) {
+  async findAll(paginationDto: PaginationDto, search?: string) {
+    let whereCondition: Prisma.UserWhereInput = {
+      isDeleted: false,
+    };
+    if (search) {
+      whereCondition = {
+        ...whereCondition,
+        OR: [
+          {
+            firstName: {
+              contains: search,
+              mode: 'insensitive',
+            },
+          },
+          {
+            lastName: {
+              contains: search,
+              mode: 'insensitive',
+            },
+          },
+          {
+            userName: {
+              contains: search,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      };
+    }
     const { offset = 0, limit = 10 } = paginationDto;
     const [total, dbUsers] = await Promise.all([
       this._prisma.user.count({ where: { isDeleted: false } }),
       this._prisma.user.findMany({
-        where: { isDeleted: false },
+        where: whereCondition,
         include: {
           roles: {
-            select: { role: true }
+            select: { role: true },
           },
         },
         skip: offset,
-        take: limit
-      })
-    ])
-    const result = dbUsers.map(user => (UserWithRolesDto.mapFrom(user)));
-    const prev = (offset - limit >= 0) ? `${this.serverUrl}/api/v1/user?offset=${offset - limit}&limit=${limit}` : null;
-    const next = (offset + limit < total) ? `${this.serverUrl}/api/v1/user?offset=${offset + limit}&limit=${limit}` : null;
-    const pagination = { total, limit, prev, next }
-    return new PaginationResultDto(pagination, result)
+        take: limit,
+      }),
+    ]);
+    const result = dbUsers.map((user) => UserWithRolesDto.mapFrom(user));
+    const prev =
+      offset - limit >= 0
+        ? `${this.serverUrl}/api/v1/user?offset=${offset - limit}&limit=${limit}`
+        : null;
+    const next =
+      offset + limit < total
+        ? `${this.serverUrl}/api/v1/user?offset=${offset + limit}&limit=${limit}`
+        : null;
+    const pagination = { total, limit, prev, next };
+    return new PaginationResultDto(pagination, result);
   }
 
   async findOne(id: number) {
@@ -72,18 +118,20 @@ export class UserService {
       where: { id, isDeleted: false },
       include: {
         roles: {
-          select: { role: true }
+          select: { role: true },
         },
-      }
+      },
     });
-    if (!existingUser) throw new NotFoundException(`There is no user with ID: ${id}`)
+    if (!existingUser)
+      throw new NotFoundException(`There is no user with ID: ${id}`);
     return UserWithRolesDto.mapFrom(existingUser);
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
     //TODO: GET CURRENT USER
     const userDb = await this.findOne(id);
-    if (userDb.email !== updateUserDto.email) await this.checkNotRegisteredEmail(updateUserDto.email);
+    if (userDb.email !== updateUserDto.email)
+      await this.checkNotRegisteredEmail(updateUserDto.email);
     const roleIds = updateUserDto.roles.map(({ id }) => id);
     await this.checkExistingRoles(roleIds);
 
@@ -94,18 +142,16 @@ export class UserService {
         changedBy: 'system',
         roles: {
           deleteMany: {},
-          create: roleIds.map(id => (
-            {
-              changedBy: 'system',
-              role: {
-                connect: { id }
-              }
-            }
-          ))
-        }
-      }
-    })
-    return UserDto.mapFrom(userUpdated)
+          create: roleIds.map((id) => ({
+            changedBy: 'system',
+            role: {
+              connect: { id },
+            },
+          })),
+        },
+      },
+    });
+    return UserDto.mapFrom(userUpdated);
   }
 
   async remove(id: number) {
@@ -113,49 +159,59 @@ export class UserService {
     await this.findOne(id);
     const { isDeleted } = await this._prisma.user.update({
       where: { id },
-      data: { isDeleted: true, changedBy: 'system' }
-    })
+      data: { isDeleted: true, changedBy: 'system' },
+    });
     return isDeleted ? true : false;
   }
 
   async updatePassword(updatePasswordDto: UpdatePasswordDto) {
     const { userId, newPassword } = updatePasswordDto;
-    await this.findOne(userId)
+    await this.findOne(userId);
     const userDb = await this._prisma.user.findUnique({
       where: { id: userId, isDeleted: false },
-      select: { password: true, email: true }
+      select: { password: true, email: true },
     });
-    if (this._bcryptAdapter.compare(updatePasswordDto.newPassword, userDb.password)) {
-      throw new BadRequestException('The new password cannot be the same as the previous passwords');
+    if (
+      this._bcryptAdapter.compare(
+        updatePasswordDto.newPassword,
+        userDb.password,
+      )
+    ) {
+      throw new BadRequestException(
+        'The new password cannot be the same as the previous passwords',
+      );
     }
     updatePasswordDto.newPassword = this._bcryptAdapter.hash(newPassword);
     const userUpdated = await this._prisma.user.update({
-      where: { id: userId},
+      where: { id: userId },
       data: {
-        password: updatePasswordDto.newPassword
-      }
-    })
+        password: updatePasswordDto.newPassword,
+      },
+    });
     return true;
   }
 
-
   private async checkNotRegisteredEmail(email: string) {
     const existingUser = await this._prisma.user.findFirst({
-      where: { email, isDeleted: false }
-    })
-    if (existingUser) throw new BadRequestException(`There is already a user with the email ${email}`);
+      where: { email, isDeleted: false },
+    });
+    if (existingUser)
+      throw new BadRequestException(
+        `There is already a user with the email ${email}`,
+      );
   }
-
 
   private async checkExistingRoles(roleIds: number[]) {
     const dbPermissions = await this._prisma.role.findMany({
       where: {
         isDeleted: false,
-        id: { in: roleIds }
-      }
-    })
+        id: { in: roleIds },
+      },
+    });
     if (roleIds.length !== dbPermissions.length) {
-      throw new BadRequestException(`At least one Role provided does not exists in the database`);
+      throw new BadRequestException(
+        `At least one Role provided does not exists in the database`,
+      );
     }
     return dbPermissions;
   }
