d53d5cc69bff03d558df2c7fdde20ae6df2ff160
Luis Murrugarra <luis@combativa.com>
2024-03-16 06:50:33 -0500

    add: swagger docs - uploads

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -7,14 +7,65 @@ import {
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBadRequestResponse, ApiBearerAuth,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Auth } from '../auth/decorators';
+import { Permissions } from '../helpers/constants';
 
 @Controller({
   path: 'uploads',
   version: '1',
 })
+@ApiTags('Uploads')
 export class UploadsController {
   constructor(private readonly uploadService: UploadService) {}
   @Post('image')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Upload Image',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiCreatedResponse({
+    description: 'The image has been uploaded successfully.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      oneOf: [
+        {
+          type: 'object',
+          properties: {
+            message: { type: 'string', example: 'No file was provided.' },
+          },
+        },
+        {
+          type: 'object',
+          properties: {
+            message: {
+              type: 'string',
+              example: 'Invalid file type. Only images are allowed.',
+            },
+          },
+        },
+        {
+          type: 'object',
+          properties: {
+            message: {
+              type: 'string',
+              example: 'File size exceeds the limit of 4MB.',
+            },
+          },
+        },
+      ],
+    },
+  })
+  @Auth(Permissions.MANAGE_RAFFLE)
   @UseInterceptors(FileInterceptor('image'))
   async uploadImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
