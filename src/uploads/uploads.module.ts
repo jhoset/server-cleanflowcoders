5ae70795c6e3e2a863bb66f226f88ee769648862
Jhoset NG <adm_dng@hotmail.com>
2024-03-16 15:44:32 -0500

    hotfix: missing auth module import

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadsController } from './uploads.controller';
+import { AuthModule } from '../auth/auth.module';
 @Module({
+  imports: [ AuthModule ],
   providers: [UploadService],
   controllers: [UploadsController],
 })
